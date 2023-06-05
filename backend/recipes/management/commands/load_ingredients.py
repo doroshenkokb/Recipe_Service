79cc8cc56ce20ed212594cf1b3dbda8447c81733
Кирилл Дорошенко <doroshenko_kirill@bk.ru>
2023-06-05 16:05:11 +0300

    modified:api/urls,utils

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -23,4 +23,4 @@ class Command(BaseCommand):
                 for row in reader
             ]
             Ingredients.objects.bulk_create(ingredients)
-        self.stdout.write(self.style.SUCCESS('=== Теги успешно загружены ==='))
+        self.stdout.write(self.style.SUCCESS('=== Ингредиенты успешно загружены ==='))
